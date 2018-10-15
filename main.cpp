ccbfb813b064caf6b5e5ae21a983f496196bf24c
Steven Zhao <37719422+Swjzhao@users.noreply.github.com>
2018-10-15 12:52:16 -0400

    upload code

---
+++ main.cpp
@@ -0,0 +1,387 @@
+//
+//  main.cpp skeleton
+//  lab3
+//
+//  Created by Tarek Abdelrahman on 2018-08-25.
+//  Copyright © 2018 Tarek Abdelrahman. All rights reserved.
+//
+
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+#include "globals.h"
+#include "shape.h"
+
+// This is the shape array, to be dynamically allocated
+shape** shapesArray;
+
+// The number of shapes in the database, to be incremented 
+// everytime a shape is successfully created
+int shapeCount = 0;
+
+// The value of the argument to the maxShapes command
+int max_shapes;
+
+// ECE244 Student: you may want to add the prototype of
+// helper functions you write here
+
+int main() {
+
+	string line;
+	string command;
+
+	string in_name;
+	string in_type;
+
+	int ints[5];
+
+	//loc loc size size angle
+
+	string errors[] = { "invalid command", "invalid argument", "invalid shape name", "exists" ,
+		"not found", "invalid shape type", "invalid value" , "too many arguments", "too few arguments",
+		"shape array is full" };
+
+	cout << "> ";         // Prompt for input
+	getline(cin, line);   // Get a line from standard input
+
+	while (!cin.eof()) {
+		// Put the line in a linestream for parsing
+		// Making a new sstream for each line so the flags are cleared
+		stringstream lineStream(line);
+
+		// Read from string stream into the command
+		// The only way this can fail is if the eof is encountered
+		lineStream >> command;
+
+		// Check for the command and act accordingly
+		// ECE244 Student: Insert your code here
+
+		if (command == keyWordsList[1]) { //maxShapes
+			if (lineStream.eof()) {
+				cout << "Error: " << errors[8] << endl;
+
+			}
+			else {
+				lineStream >> max_shapes;
+				if (lineStream.fail()) {
+					cout << "Error: " << errors[1] << endl;
+				}
+				else if (!lineStream.eof()) {
+					cout << "Error: " << errors[8] << endl;
+				}
+				else {
+					shapesArray = new shape*[max_shapes];
+					for (int i = 0; i < max_shapes; i++)
+						shapesArray[i] = nullptr;
+					cout << "New database: max shapes is " << max_shapes << endl;
+				}
+			}
+		}
+		else if (command == keyWordsList[2]) { //create
+			if (lineStream.eof()) {
+				cout << "Error: " << errors[8] << endl;
+
+			}
+			else {
+				lineStream >> in_name;
+				if (lineStream.eof()) {
+					cout << "Error: " << errors[8] << endl;
+
+				}
+				else {
+					bool isInvalid = false;
+					bool isUsed = false;
+					for (string s : keyWordsList) {
+						if (s == in_name)
+							isInvalid = true;
+					}
+					for (string s : shapeTypesList) {
+						if (s == in_name)
+							isInvalid = true;
+					}
+
+					for (int i = 0; i < max_shapes; i++) {
+						if (shapesArray[i] != nullptr && shapesArray[i]->getName() == in_name) {
+							isUsed = true;
+						}
+					}
+
+					if (isInvalid) {
+						cout << "Error: " << errors[2] << endl;
+					}
+					else if (isUsed) {
+						cout << "Error: shape " << in_name << " " << errors[3] << endl;
+					}
+					else {
+
+						lineStream >> in_type;
+						if (in_type != shapeTypesList[0] &&
+							in_type != shapeTypesList[1] &&
+							in_type != shapeTypesList[2]) {
+
+							cout << "Error: " << errors[5] << endl;
+						}
+						else {
+							if (lineStream.eof()) {
+								cout << "Error: " << errors[8] << endl;
+							}
+							else {
+								lineStream >> ints[0];
+								if (lineStream.fail()) {
+									cout << "Error: " << errors[1] << endl;
+								}
+								else {
+									if (lineStream.eof()) {
+										cout << "Error: " << errors[8] << endl;
+									}
+									else {
+										lineStream >> ints[1];
+										if (lineStream.fail()) {
+											cout << "Error: " << errors[1] << endl;
+										}
+										else {
+											if (lineStream.eof()) {
+												cout << "Error: " << errors[8] << endl;
+											}
+											else {
+												lineStream >> ints[2];
+												if (lineStream.fail()) {
+													cout << "Error: " << errors[1] << endl;
+												}
+												else {
+													if (lineStream.eof()) {
+														cout << "Error: " << errors[8] << endl;
+													}
+													else {
+														lineStream >> ints[3];
+														if (lineStream.fail()) {
+															cout << "Error: " << errors[1] << endl;
+														}
+														else {
+															if (lineStream.eof()) {
+																if (shapeCount >= max_shapes) {
+																	cout << "Error: " << errors[9] << endl;
+																}
+																else {
+																	shapesArray[shapeCount++] = new shape(in_name, in_type, ints[0], ints[1], ints[2], ints[3]);
+																	cout << "Created " << in_name << " " << in_type << " " << ints[0] << " " << ints[1]
+																		<< " " << ints[2] << " " << ints[3] << endl;
+																	shapeCount++;
+																}
+															}
+															else {
+																cout << "Error: " << errors[7] << endl;
+															}
+
+														}
+
+													}
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		else if (command == keyWordsList[3]) { //move
+			if (lineStream.eof()) {
+				cout << "Error: " << errors[8] << endl;
+
+			}
+			else {
+				lineStream >> in_name;
+				if (lineStream.eof()) {
+					cout << "Error: " << errors[8] << endl;
+				}
+				else {
+					bool isFound = false;
+					for (int i = 0; i < max_shapes; i++) {
+						if (shapesArray[i]->getName() == in_name) {
+							isFound = true;
+
+							if (lineStream.eof()) {
+								cout << "Error: " << errors[8] << endl;
+							}
+							else {
+								lineStream >> ints[0];
+								if (lineStream.fail()) {
+									cout << "Error: " << errors[1] << endl;
+								}
+								else if (!lineStream.eof()) {
+									cout << "Error: " << errors[8] << endl;
+								}
+								else {
+									lineStream >> ints[1];
+									if (lineStream.fail()) {
+										cout << "Error: " << errors[1] << endl;
+									}
+									else if (lineStream.eof()) {
+										cout << "Error: " << errors[7] << endl;
+									}
+									else {
+										shapesArray[i]->setXlocation(ints[0]);
+										shapesArray[i]->setXlocation(ints[1]);
+										cout << "Moved " << shapesArray[i]->getName() << " to " << ints[0] << " " << ints[1] << endl;
+									}
+								}
+
+							}
+
+
+							break;
+						}
+					}
+					if (!isFound) {
+						cout << "Error: shape " << in_name << " " << errors[4] << endl;
+					}
+				}
+			}
+		}
+		else if (command == keyWordsList[4]) { //rotate
+			if (lineStream.eof()) {
+				cout << "Error: " << errors[8] << endl;
+
+			}
+			else {
+				lineStream >> in_name;
+				if (lineStream.eof()) {
+					cout << "Error: " << errors[8] << endl;
+				}
+				else {
+					bool isFound = false;
+					for (int i = 0; i < max_shapes; i++) {
+						if (shapesArray[i]->getName() == in_name) {
+							isFound = true;
+
+							if (lineStream.eof()) {
+								cout << "Error: " << errors[8] << endl;
+							}
+							else {
+
+								lineStream >> ints[4];
+								if (lineStream.fail()) {
+									cout << "Error: " << errors[1] << endl;
+								}
+								else if (lineStream.eof()) {
+									cout << "Error: " << errors[7] << endl;
+								}
+								else {
+									shapesArray[i]->setRotate(ints[4]);
+									cout << "Rotated " << shapesArray[i]->getName() << "by " << ints[4] << " degrees" << endl;
+								}
+
+
+							}
+
+
+							break;
+						}
+					}
+					if (!isFound) {
+						cout << "Error: shape " << in_name << " " << errors[4];
+					}
+				}
+			}
+		}
+		else if (command == keyWordsList[5]) { //draw
+			if (lineStream.eof()) {
+				cout << "Error: " << errors[8] << endl;
+
+			}
+			else {
+				lineStream >> in_name;
+				if (!lineStream.eof()) {
+					cout << "Error: " << errors[7] << endl;
+				}
+				else {
+					if (in_name == keyWordsList[0]) {
+						cout << "Drew all shapes" << endl;
+						for (int i = 0; i < max_shapes; i++)
+							if (shapesArray[i] != nullptr)
+								shapesArray[i]->draw();
+
+
+
+					}
+					else {
+						bool isFound = false;
+
+						for (int i = 0; i < max_shapes; i++) {
+							if (shapesArray[i]->getName() == in_name) {
+								isFound = true;
+								shapesArray[i]->draw();
+
+								break;
+							}
+						}
+
+						if (!isFound) {
+							cout << "Error: shape " << in_name << " " << errors[4];
+						}
+					}
+				}
+			}
+		}
+		else if (command == keyWordsList[6]) { //delete
+
+			if (lineStream.eof()) {
+				cout << "Error: " << errors[8] << endl;
+
+			}
+			else {
+				lineStream >> in_name;
+				if (!lineStream.eof()) {
+					cout << "Error: " << errors[7] << endl;
+				}
+				else {
+					if (in_name == keyWordsList[0]) {
+						cout << "Deleted all shapes" << endl;
+						for (int i = 0; i < max_shapes; i++)
+							if (shapesArray[i] != nullptr)
+								delete shapesArray[i];
+
+
+
+					}
+					else {
+						bool isFound;
+
+						for (int i = 0; i < max_shapes; i++) {
+							if (shapesArray[i]->getName() == in_name) {
+								isFound = false;
+								delete shapesArray[i];
+
+								break;
+							}
+						}
+
+						if (!isFound) {
+							cout << "Error: shape " << in_name << " " << errors[4];
+						}
+					}
+				}
+			}
+		}
+		else {
+			cout << "Error: " << errors[0];
+		}
+
+
+
+		// Once the command has been processed, prompt for the
+		// next command
+		cout << "> ";          // Prompt for input   
+		getline(cin, line);   // Get the command line
+
+	}  // End input		loop until EOF.
+
+	return 0;
+}
+
---
+++ shape.cpp
@@ -0,0 +1,93 @@
+//
+//  shape.cpp skeleton
+//  lab3
+//
+//  Created by Tarek Abdelrahman on 2018-08-25.
+//  Copyright © 2018 Tarek Abdelrahman. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include "shape.h"
+
+// ECE244 Student: 
+// Write the implementation (and only the implementation) of the shape
+// class below
+
+shape::shape(string n, string t, int x_loc, int y_loc, int x_sz, int y_sz) {
+	type = t;
+	name = n;
+	x_location = x_loc;
+	y_location = y_loc;
+	x_size = x_sz;
+	y_size = y_sz;
+
+}
+
+string shape:: getType() {
+	// The name of the shape
+	
+	return type;
+}
+string shape:: getName() {
+	// The type of the shape (see globals.h)
+	return name;
+}
+int shape:: getXlocation() {
+	// Returns location of the shape on the x-axis
+	return x_location;
+}
+int shape:: getYlocation() {
+	// Returns location of the shape on the y-axis
+	return y_location;
+}
+int shape:: getXsize() {
+	// Returns the size of the shape in the x-dimension
+	return x_size;
+}
+int shape:: getYsize() {
+	// Returns the size of the shape in the y-dimension
+	return y_size;
+}
+// Mutators
+void shape::setType(string t) {       
+	// Sets the type (see globals.h)
+	// No error checking done inside the method
+	// The calling program must ensure the type
+	// is correct
+	type = t;
+
+}
+void shape::setName(string n) {       
+	// Sets the name of the shape
+	name = n;
+}
+void shape::setXlocation(int x_loc) {
+	// Sets location of the shape on the x-axis
+	x_location = x_loc;
+}
+void shape::setYlocation(int y_loc) {
+	// Sets location of the shape on the y-axis
+	y_location = y_loc;
+}
+void shape::setXsize(int x_sz) {
+	// Sets size of the shape in the x-dimension
+	x_size = x_sz;
+}
+void shape::setYsize(int y_sz) {
+	// Sets size of the shape in the y-dimension
+	y_size = y_sz;
+}
+void shape::setRotate(int angle) {
+	// sets the rotation of the shape
+	rotation = angle;
+}
+// Utility methods
+void shape::draw() {
+	cout << type << " " << getXlocation() << " "
+		<< getYlocation() << " " << getXsize() << " " << getYsize() << endl;
+
+
+}
